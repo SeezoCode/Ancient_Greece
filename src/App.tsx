0a82cd170543815158546701809b64ac0e7e560c
Filip <47699264+SeezoCode@users.noreply.github.com>
2021-04-07 20:25:21 +0200

    All but one site done

--- src/App.tsx
+++ src/App.tsx
@@ -1,152 +1,191 @@
 import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react'
-
-
-interface SectionInterface {
-  h1?: string,
-  header: string,
-  paragraphs: Array<string>,
-  image?: string,
-  customHTMLs: Array<string>
-}
+import {Component} from 'react'
 
 
 interface StateInterface {
-  current: number,
-  cache: Array<NavBarElemInterface>
+    current: number,
+    cache: Array<NavBarElemInterface>
 }
 
 interface NavBarElemInterface {
-  text: string,
-  dataAt: string,
-  pendingFetch: boolean,
-  savedData?: Array<SectionInterface>
+    text: string,
+    dataAt: string,
+    type: string,
+    savedData?: string,
+}
+
+interface landingInterface {
+    img: string,
+    heading: string,
+    description: string,
+    key: number
 }
 
 let navBarElems = [
-  {
-    text: 'Roman Empire',
-    pendingFetch: false,
-    dataAt: 'http://127.0.0.1:8080/roman_empire.json'
-  },
-  {
-    text: 'History',
-    pendingFetch: false,
-    dataAt: 'http://127.0.0.1:8080/history.json'
-  },
-  {
-    text: 'Geography',
-    pendingFetch: false,
-    dataAt: 'http://127.0.0.1:8080/geography.json'
-  },
+    {
+        text: 'Roman Empire',
+        type: 'landingPage',
+        dataAt: 'http://127.0.0.1:8080/roman_empire.json'
+    },
+    {
+        text: 'History',
+        type: 'html',
+        dataAt: 'http://127.0.0.1:8080/history.html'
+    },
+    {
+        text: 'Geography',
+        type: 'html',
+        dataAt: 'http://127.0.0.1:8080/geography.html'
+    },
+    {
+        text: 'Philosophers',
+        type: 'json',
+        dataAt: 'http://127.0.0.1:8080/philosophers.json'
+    },
 ]
 
 
-
-async function getDescription(link: string): Promise<Array<SectionInterface>> {
-  let response = await fetch(link)
-  return response.json()
+async function getDescription(link: string): Promise<string> {
+    let response = await fetch(link)
+    return response.text()
 }
 
 
 class App extends Component {
-  state: StateInterface
+    state: StateInterface
 
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      current: 0,
-      cache: navBarElems,
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            current: 0,
+            cache: navBarElems,
+        }
+        this.fetchContentData = this.fetchContentData.bind(this)
+        this.changeSection = this.changeSection.bind(this)
     }
-    this.fetchContentData = this.fetchContentData.bind(this)
-  }
 
-  componentDidMount() {
-    this.fetchContentData(0)
-  }
-
-  fetchContentData(key: number) {
-    console.log(key, this.state)
-    let currentContext = this.state.cache[key]
-    if (currentContext.savedData) {
-      this.setState({current: key})
-      console.log('from cache')
+    componentDidMount() {
+        this.fetchContentData(0)
     }
-    else {
-      getDescription(currentContext.dataAt).then((data) => {
-        let cache = this.state
-        cache.cache[key].savedData = data
-        this.setState(cache)
+
+    fetchContentData(key: number) {
+        console.log(key, this.state)
+        let currentContext = this.state.cache[key]
+        if (currentContext.savedData) {
+            // this.setState({current: key})
+            console.log('from cache')
+        } else {
+            getDescription(currentContext.dataAt).then((data) => {
+                let cache = this.state
+                cache.cache[key].savedData = data
+                this.setState(cache)
+            })
+        }
+    }
+
+    changeSection(key: number): void {
         this.setState({current: key})
-      })
     }
-  }
 
-  renderSections() {
-    let data = this.state.cache[this.state.current].savedData
-    if (data) return Sections(data)
-  }
+    renderSections() {
+        let data = this.state.cache[this.state.current].savedData
+        if (data) return Section({__html: data})
+    }
 
-  render() {
-    return (
-        <div className="App">
-          <NavBar fetchContentData={this.fetchContentData} />
-          <div className="container">
-            <button onClick={() => {this.fetchContentData(1)}}>change</button>
-            {this.renderSections()}
-          </div>
-        </div>
-    );
-  }
-}
+    landingPage = () => {
+        let data = this.state.cache[this.state.current].savedData
+        if (data) {
+            let parsed = JSON.parse(data)
+            return LandingElems(parsed, this.fetchContentData, this.changeSection)
+        }
+    }
 
-
-function Sections(state: Array<SectionInterface>) {
-  let secs = state.map((elem, key) => {
-    return <div key={key}>{Section(elem)} {elem.image && <img src={elem.image} />}</div>
-  })
-  return (
-      <div>
-        <h1>{state[0]["h1"]}</h1>
-        {secs}
-      </div>
-  )
-}
-
-function Section(state: SectionInterface) {
-  let pars = state.paragraphs.map((par, key) => {
-    return <p key = {key}>{par}</p>
-  })
-  return (
-      <div>
-        <h2>{state.header}</h2> <hr />
-        {pars}
-      </div>
-  )
-}
-
-
-class NavBar extends Component<any, any>{
-  buildSections() {
-    let lis = navBarElems.map((elem, key) => {
-      return <li key={key} className='nav-item'>
-        <button className='btn btn-light' onClick={() => this.props.fetchContentData(key)}>{elem.text}</button></li>
-    })
-    return <ul className="navbar-nav">{lis}</ul>
-  }
-
-  render() {
-    return (
-        <nav className="navbar navbar-expand-sm navbar-light bg-light">
-          <div className="container">
-            <div className="collapse navbar-collapse" id="navbarNav">
-              {this.buildSections()}
+    render() {
+        return (
+            <div className="App">
+                <NavBar fetchContentData={this.fetchContentData} changeSection={this.changeSection}/>
+                {/*<div className="container">*/}
+                {navBarElems[this.state.current].type === 'html' && this.renderSections()}
+                {navBarElems[this.state.current].type === 'landingPage' && this.landingPage()}
+                <Footer />
+                {/*{this.state.current === 0 && InitialCards(navBarElems)}*/}
+                {/*</div>*/}
             </div>
-          </div>
-        </nav>
-    );
-  }
+        );
+    }
+}
+
+
+function Section(state: any) {
+    return (
+        // @ts-ignore
+        <div dangerouslySetInnerHTML={state}/>
+    )
+}
+
+function LandingElems(data: Array<landingInterface>, fetchContentData: any, changeSection: any) {
+    let array = [[], [], []]
+    for (let i = 0; i < data.length; i++) {
+        // @ts-ignore
+        array[i % 3][Math.floor(i / 3)] = <div className="col" key={i}>{LandingElem(data[i], fetchContentData, changeSection)}</div>
+    }
+    console.log(array)
+    return <div className="row landing">
+        <div className="col-4 landingCol">{array[0]}</div>
+        <div className="col-4 landingCol">{array[1]}</div>
+        <div className="col-4 landingCol">{array[2]}</div>
+    </div>
+}
+
+
+function LandingElem(data: landingInterface, fetchContentData: any, changeSection: any) {
+    return (
+        <div className="landingElem">
+            <img src={data.img} className="landingImages"/>
+            <h4>{data.heading}</h4>
+            <p>{data.description}</p>
+            <button className="btn btn-light" onMouseOver={() => fetchContentData(data.key)} onClick={() => changeSection(data.key)}>See more
+            </button>
+        </div>
+    )
+}
+
+
+function Footer() {
+    return (
+        <footer>
+            <br />
+            <p>This is the footer</p>
+            <br />
+            <br />
+        </footer>
+    )
+}
+
+
+class NavBar extends Component<any, any> {
+    buildSections() {
+        let lis = navBarElems.map((elem, key) => {
+            return <li key={key} className='nav-item'>
+                <button className='btn btn-dark' onMouseOver={() => this.props.fetchContentData(key)}
+                        onClick={() => this.props.changeSection(key)}>{elem.text}</button>
+            </li>
+        })
+        return <ul className="navbar-nav">{lis}</ul>
+    }
+
+    render() {
+        return (
+            <nav className="navbar navbar-expand navbar-dark bg-dark">
+                <div className="container">
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        {this.buildSections()}
+                    </div>
+                </div>
+            </nav>
+        );
+    }
 }
 
 
